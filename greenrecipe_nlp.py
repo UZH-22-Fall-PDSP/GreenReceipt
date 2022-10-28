a9216c47c000576a4f84c430c7f26a5a52f64bfe
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2022-10-28 14:44:32 +0200

    Add again temp nlp code and comment it out

--- greenrecipe_nlp.py
+++ greenrecipe_nlp.py
@@ -41,3 +41,21 @@ def find_similar_ing(ingredList):
 		
 	return  ingredList
 
+
+
+
+def find_similar_ing(ingrdList):
+    # Input : <str: Ingredient Name>
+    # Output : List [<str: Ingredient Name>, <str: Ingredient Matched>]
+    ingrdList = [{'ingredient': 'peas', 'quantity': 1.0, 'unit': 'ea'}
+            , {'ingredient': 'runner beans', 'quantity': 56.69, 'unit': 'g'}
+            , {'ingredient': 'beef cold cuts', 'quantity': 14.79, 'unit': 'ml'}
+            , {'ingredient': 'apple juice', 'quantity': 1.0, 'unit': 'ea'}
+            , {'ingredient': 'peach', 'quantity': 59.14, 'unit': 'ml'}
+            , {'ingredient': 'poppy seed', 'quantity': 14.79, 'unit': 'ml'}
+            , {'ingredient': 'fish oil', 'quantity': 0.61, 'unit': 'ml'}
+            , {'ingredient': 'celery', 'quantity': 0.61, 'unit': 'ml'}
+            , {'ingredient': 'gem squash', 'quantity': 0.3, 'unit': 'ml'}
+            , {'ingredient': 'sour cream', 'quantity': 0.3, 'unit': 'ml'}
+            , {'ingredient': 'coconut', 'quantity': 29.57, 'unit': 'ml'}]
+    return ingrdList
